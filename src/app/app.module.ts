ab112259bc4e5b78452f9fd66d268bad3748fcb6
Salih <salihkilinc80@gmail.com>
2023-05-31 14:16:35 +0200

    vs3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { Com1Component } from './com1/com1.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    Com1Component
   ],
   imports: [
     BrowserModule,
