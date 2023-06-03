4d971c032f559582e913f658d2e57e65d8c498c8
Salih <salihkilinc80@gmail.com>
2023-06-03 23:42:52 +0200

    c3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,8 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   //templateUrl: './app.component.html',
   template:`<p>Bonjour</p>
-<app-home [pageName]="title2 "> </app-home>
+
+<app-home [pageName]="title2 ">   //Property Binding</app-home>
 
   <hr>
   <div style="background-color: yellow;">
@@ -12,10 +13,22 @@ import { Component } from '@angular/core';
 <h1>{{title}}</h1>
 <input type= "text" [value]= "title">
 </div>
+
+<!--Two-Way Data Binding-->
+
+ <!-- 
+<input type="text" [(ngModel)]="name">  <br>
+<input type="text" [(ngModel)]="name">  <br>
+
+{{name}} 
+
+-->
+
   `,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'proj1';
   title2="proj2"
+  name: string 
 }
