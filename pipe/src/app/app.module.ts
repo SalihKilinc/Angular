3aa5da4b7bd48546d2872fce5f00a1a9b935ffa2
Salih <salihkilinc80@gmail.com>
2023-06-13 22:28:04 +0200

    xc5

--- pipe/src/app/app.module.ts
+++ pipe/src/app/app.module.ts
@@ -1,26 +1,24 @@
-import { NgModule } from '@angular/core';
+import { DEFAULT_CURRENCY_CODE, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-//import { Com1Component } from './com1/com1.component';
-import { Comp1Component } from './comp1/comp1.component';
 import { FormsModule } from '@angular/forms';
+import { CustomPipe } from './pipes/custom.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
+    CustomPipe
    
-    Comp1Component
+   
+  
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule
   ],
-  providers: [],
+  providers: [{provide:DEFAULT_CURRENCY_CODE, useValue : "$" }, CustomPipe],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
