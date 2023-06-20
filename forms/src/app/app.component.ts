0dad3745fcd227118f122b100200a3f709458766
Salih <salihkilinc80@gmail.com>
2023-06-20 11:30:31 +0200

    cv1

--- forms/src/app/app.component.ts
+++ forms/src/app/app.component.ts
@@ -13,6 +13,13 @@ import { NgForm } from '@angular/forms';
       <!-- ngModel ile etiketledik ve consol degerini aldik 
 onSubmiti export kismindan data ile cagirdik
 -->
+<div ngModelGroup="address">
+ <input type="text" name="Country" placeholder="Country" ngModel> <br>
+ <input type="text" name="City" placeholder="City" ngModel> <br>
+ <input type="text" name="address" placeholder="address" ngModel> <br>
+<!-- adress kismini obje olarak verdi consolda ngModelGrup ozelligi -->
+</div>
+
       <button>Send</button>
     </form>
   `,
@@ -21,10 +28,13 @@ export class AppComponent {
   @ViewChild("frm", {static:true}) frm :NgForm;  // frm kismini aldik static olarak
   onSubmit(data) {
    // console.log(this.frm.value)
-   console.log(`Value : ${this.frm.value}`)
-   console.log(`Valid : ${this.frm.valid}`)
-   console.log(`Touched : ${this.frm.touched}`)  // bir input bile dolu olsa true verir
-   console.log(`Submited : ${this.frm.submitted}`) 
-   console.log(data);
+   //console.log(`Value : ${this.frm.value}`)
+   //console.log(`Valid : ${this.frm.valid}`)
+   //console.log(`Touched : ${this.frm.touched}`)  // bir input bile dolu olsa true verir
+   //console.log(`Submited : ${this.frm.submitted}`)  // send butonu aktif mi 
+   console.log(this.frm)
+   console.log(this.frm.form)
+   console.log(this.frm.controls)  // form kontrol taglari
+   //console.log(data);
   }
 }
---
+++ model-driven-forms/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  //templateUrl: './app.component.html',
+  template:`
+
+
+  `,
+
+
+})
+export class AppComponent {
+  
+
+}
+
+
+
