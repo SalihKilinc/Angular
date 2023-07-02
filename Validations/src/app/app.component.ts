581b2927fa493693d87039f9efc40d381a08ede7
Salih <salihkilinc80@gmail.com>
2023-07-02 14:37:32 +0200

    cx2

--- Validations/src/app/app.component.ts
+++ Validations/src/app/app.component.ts
@@ -1,19 +1,33 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   //templateUrl: './app.component.html',
-  template:`<p>Bonjour</p>
-
-
-  `,
-
-
+  template: `
+    <form [formGroup]="frm" (ngSubmit)="onSubmit()">
+      <input type="text" placeholder="Name" formControlName="name" /> <br />
+      <input type="text" placeholder="Surname" formControlName="surname" /> <br />
+      <input type="text" placeholder="Email" formControlName="email" /> <br />
+      <input type="text" placeholder="Tel" formControlName="tel" /> <br />
+      <button type="submit">Send</button>
+    </form>
+  `
 })
 export class AppComponent {
-  
+  frm: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) {
+    this.frm = this.formBuilder.group({
+      name: ['', [Validators.required , Validators.minLength(3)] ], // requerid olarak en az uc harf belirldik
+      surname: [''],
+      email: [''],
+      tel: ['']
+    });
+  }
+
+  onSubmit() {
+    console.log(this.frm.valid)
+    console.log(this.frm.value);
+  }
 }
-
-
-
