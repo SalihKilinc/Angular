a6106b5cc1d8d33649362e1fa4d58da7c9fd98fe
Salih <salihkilinc80@gmail.com>
2023-07-02 15:10:25 +0200

    cvx2

--- Validations/src/app/app.component.ts
+++ Validations/src/app/app.component.ts
@@ -7,27 +7,44 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   template: `
     <form [formGroup]="frm" (ngSubmit)="onSubmit()">
       <input type="text" placeholder="Name" formControlName="name" /> <br />
-      <input type="text" placeholder="Surname" formControlName="surname" /> <br />
+      <div *ngIf="!name.valid && (name.dirty || name.touched)"> {{name.errors | json}} </div>
+      
+      <input type="text" placeholder="Surname" formControlName="surname" /><br />
+      <div *ngIf="!surname.valid && (surname.dirty || surname.touched)"> {{surname.errors | json}} </div>
+
       <input type="text" placeholder="Email" formControlName="email" /> <br />
+      <div *ngIf="!email.valid && (email.dirty || email.touched)"> {{email.errors | json}} </div>
+
       <input type="text" placeholder="Tel" formControlName="tel" /> <br />
       <button type="submit">Send</button>
     </form>
-  `
+  `,
 })
 export class AppComponent {
   frm: FormGroup;
 
   constructor(private formBuilder: FormBuilder) {
     this.frm = this.formBuilder.group({
-      name: ['', [Validators.required , Validators.minLength(3)] ], // requerid olarak en az uc harf belirldik
-      surname: [''],
-      email: [''],
-      tel: ['']
+      name: ['', [Validators.required, Validators.minLength(3)]], // requerid olarak en az uc harf belirldik
+      surname: ['', [Validators.required , Validators.minLength(2)] ],
+      email: ['',[Validators.required , Validators.email] ],
+      tel: [''],
     });
   }
-
+  get name() {
+    return this.frm.get('name');
+  }
+  get surname() {
+    return this.frm.get('surname');
+  }
+  get email() {
+    return this.frm.get('email');
+  }
+  get tel() {
+    return this.frm.get('tel');
+  }
   onSubmit() {
-    console.log(this.frm.valid)
+    console.log(this.frm.valid);
     console.log(this.frm.value);
   }
 }
